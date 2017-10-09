1e8512d7b92bd6d747a0493db3deaf4030b49254
Eugene Oparyshev <eop@sitecore.net>
2017-10-09 10:39:40 +0300

    Github Patch

---
+++ src/Sitecore.Support.90160/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.90160")]
+[assembly: AssemblyProduct("Sitecore.Support.90160")]
+[assembly: ComVisible(false)]
